9f3fad6149fbc41645d3e05deb04b0efaf378cf7
siningtong <siningtong@gmail.com>
2020-01-15 06:21:47 +0000

    css the sidebar to the left

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./JokeList.css";
 const URL = " https://icanhazdadjoke.com/";
 class JokeList extends Component {
   static defaultProps = { numJokesToget: 10 };
@@ -13,17 +14,25 @@ class JokeList extends Component {
       let res = await axios.get(URL, {
         headers: { Accept: "application/json" }
       });
-      jokes.push(res.data.joke);
+      jokes.push({ joke: res.data.joke, votes: 0 });
     }
     this.setState({ jokes: jokes });
   }
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span>Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(joke => (
-            <div>{joke}</div>
+          {this.state.jokes.map(j => (
+            <div>
+              {j.joke}-{j.votes}
+            </div>
           ))}
         </div>
       </div>
